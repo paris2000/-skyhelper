888e43e8e82306c0b62174f95308e8b7d89f6b7f
paris2000 <30391698+paris2000@users.noreply.github.com>
2017-07-25 02:01:54 +0800

    respone

---
+++ respon.js
@@ -0,0 +1,15 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+    channelId:1526441000,
+    channelSecred :66c56d9328699fdd93d9dbc0054c70a2,
+    channelAccessToken:lPxdIo6+sv9xFQfXi3LZLjBEI9Cx8xSxfYMTn/E700pDsVNXDZvXi6PXnLvL/E5yTsmrAJ6tQaRAFP+vympkcR4tajnoskwFyQg4vt5QCbT73IzjagV+089GPqYoOL3h9gW+EWbdcme/eOYOY6BZgQdB04t89/1O/w1cDnyilFU=
+});
+
+setTimeout(function(){
+    var userId = '使用者 ID';
+    var sendMsg = '要發送的文字';
+    bot.push(userId,sendMsg);
+    console.log('send: '+sendMsg);
+},5000);
